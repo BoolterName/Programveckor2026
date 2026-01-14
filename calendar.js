de4160515558b8cacfa6601ee9ab8a30c01347e9
lanapplity <olof.akerfeldt@learnet.se>
2026-01-14 12:59:41 +0100

    Skolstyrelsen valde att ha 100 prov per dag

--- calendar.js
+++ calendar.js
@@ -26,6 +26,9 @@ const currentMonthElement = document.querySelector("#currentmonth");
 
 const backBtn = document.querySelector("#back");
 const forwardBtn = document.querySelector("#forward");
+const createBtn = document.querySelector("#create");
+
+
 
 const locale = "sv-SE";
 let currentMonth = Temporal.Now.plainDateISO();
@@ -33,22 +36,21 @@ let currentMonth = Temporal.Now.plainDateISO();
 function getRandomInt(min, max){
     return min + Math.floor(Math.random() * (max - min));
 }
-let assignments = []
+const assignments = [];
 
 class assignment{
-    constructor(name, description, date) {
+    constructor(name, color , description, date) {
         this.name = name;
+        this.color = color
         this.description = description;
         this.date = date;
         assignments.push(this);
     }
 }
 
-new assignment("Prov!", "lorem och ipsum",Temporal.Now.plainDateTimeISO().with({day:getRandomInt(1,20)}))
-
-new assignment("Annat Prov", "lorem och ipsum",Temporal.Now.plainDateTimeISO().with({day:getRandomInt(1,20)}))
-
-new assignment("Tredje Prov", "lorem och ipsum",Temporal.Now.plainDateTimeISO().with({day:getRandomInt(1,20)}))
+for (let index = 0; index < 30; index++) {
+    new assignment("Prov!", ["#ff8080","#90ff80","#ffdf80"][getRandomInt(0,3)],"lorem och ipsum",Temporal.Now.plainDateTimeISO().add({days:getRandomInt(-20,20)}))
+}
 
 
 
@@ -68,6 +70,12 @@ forwardBtn.addEventListener("click", () => {
     currentMonth = currentMonth.add({months:1});
     update();
 })
+
+createBtn.addEventListener("click", () => {
+    
+})
+
+
 
 function update(){
     updateCalendarDates(currentMonth);
@@ -132,29 +140,35 @@ function updateAssignments(month){
         else{
             currentDate = firstDayInMonth.add({days:i-monthStartDayOfWeek+1});
         }
-
+        console.log(currentDate.day)
 
         const dateDiv = dateDivs[i];
-        let assignments = document.createElement("div");
-        assignments.className = "assignments";
-        dateDiv.appendChild(assignments);
+        let assignmentsDiv = document.createElement("div");
+        assignmentsDiv.className = "assignments";
+        dateDiv.appendChild(assignmentsDiv);
         // hittar alla uppgifter den dagen
+        assignments.forEach(a => {
+            if(Temporal.PlainDate.compare(a.date, currentDate) == 0){
+                let assignmentP = document.createElement("p");
+                assignmentP.textContent = a.name;
+                assignmentP.style.backgroundColor = a.color;
+                assignmentsDiv.appendChild(assignmentP);
+            }
+        });
 
-        
-
-        if (i%13 == 4)
-        {
-            let assignment = document.createElement("p");
-            assignment.textContent = "prov!";
-            assignment.style.backgroundColor = "#90ff80";
-            assignments.appendChild(assignment);
-        }
-        if (i%5  == 2 && i%7 < 5)
-        {
-            let assignment2 = document.createElement("p");
-            assignment2.textContent = "Annat prov!";
-            assignment2.style.backgroundColor = "#ff8080";
-            assignments.appendChild(assignment2);        }
-
+        // if (i%13 == 4)
+        // {
+        //     let a = document.createElement("p");
+        //     a.textContent = "prov!";
+        //     a.style.backgroundColor = "#90ff80";
+        //     assignments.appendChild(a);
+        // }
+        // if (i%5  == 2 && i%7 < 5)
+        // {
+        //     let assignment2 = document.createElement("p");
+        //     assignment2.textContent = "Annat prov!";
+        //     assignment2.style.backgroundColor = "#ff8080";
+        //     assignments.appendChild(assignment2);        
+        // }
     }
 }
